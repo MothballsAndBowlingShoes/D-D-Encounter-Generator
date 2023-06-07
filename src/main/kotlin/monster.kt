e09d2a8fe11c8dfc98d1ecb67125316c5095306c
Atticus <Atticuscyoung@icloud.com>
2023-06-06 17:21:38 -0700

    Initial Commit 2

---
+++ src/main/kotlin/monster.kt
@@ -0,0 +1,10 @@
+val name: String?
+val combatRating: Double?
+val armorClass: int?
+val hitPoints: int?
+val speed: int?
+
+class Monster (val name: String, val combatRating: Double, val armorClass: int, val hitPoints: int, val speed: int)
+{
+
+}
